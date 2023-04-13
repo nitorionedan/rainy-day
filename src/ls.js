0cd7fd15619c823f979432c995104fe3cad90f8c
nitorionedan <fearinatunitori@gmail.com>
2023-04-13 10:54:21 +0900

    Display current location

--- src/ls.js
+++ src/ls.js
@@ -1,40 +1,31 @@
+let currentLatLng = [0, 0];
+
 function quickStart() {
     // Leafletマップオブジェクトを作成
-    var sendaiPoint = [38.26764, 140.87366];
-    let map = L.map("map").setView(sendaiPoint, 14);
+    // var sendaiPoint = [38.268223, 140.869415];
+    // let map = L.map("map").setView(sendaiPoint, 7);
+
+    // 現在地を地図で表示
+    let map = L.map("map", {doubleClickZoom: false});
+    map.locate({setView: true, maxZoom: 7});
+
+    // 現在地にピン
+    let marker;
+    map.on("locationfound", (e) => {
+        if (!marker) {
+            marker = L.marker(e.latlng).addTo(map);
+        } else {
+            marker.setLatLng(e.latlng).addTo(map);
+        }
+
+        marker.bindPopup(e.latlng.toString()).openPopup();
+    });
 
     // タイルレイヤーを設定
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-
-    // ピンを地図に追加
-    let marker = L.marker(sendaiPoint).addTo(map);
-    marker.bindPopup("ok").openPopup();
-
-    // 円を地図に追加
-    let circle = L.circle(sendaiPoint, {
-        color: "red",
-        fillColor: "#f03",
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(map);
-
-    // ポリゴンを地図に追加
-    let polygon = L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ]).addTo(map);
-
-    let popup = L.popup();
-    map.on("click", (e) => {
-        console.log("at " + e.latlng);
-        popup.setLatLng(e.latlng)
-            .setContent(e.latlng.lat.toString())
-            .openOn(map);
-    });
 }
 
 function initShareButton() {
@@ -50,9 +41,9 @@ function initShareButton() {
     btn.addEventListener("click", async () => {
         try {
             await navigator.share(shareData);
-            resultPara.textContent = "Shared successfully";
+            console.log("Shared successfully");
         } catch (err) {
-            resultPara.textContent = `Error: ${err}`;
+            console.log(`Error: ${err}`);
         }
     });
 }
