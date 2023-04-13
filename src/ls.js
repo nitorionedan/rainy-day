20fca5d9fc25512531a76c2b2a5962eb65e6f1cc
nitorionedan <fearinatunitori@gmail.com>
2023-04-13 21:50:23 +0900

    Realtime locating

--- src/ls.js
+++ src/ls.js
@@ -1,11 +1,13 @@
 const today = new Date();
+const defaultZoomLvl = 16;
 
+let map = undefined;
 let currentLatLng = {
     lat: 0,
     lng: 0
 };
-
 let opponentLatLng = undefined;
+let marker = undefined;
 
 const hours = () => {
     // 表示する桁数を2桁へ調整
@@ -18,12 +20,11 @@ const minutes = () => {
 }
 
 window.onload = () => {
-    let url = location.href;
+    // URL経度緯度パラメータを解析
+    const url = location.href;
     paramLatLng = url.substring(url.indexOf("#") + 1, url.length);
-    let lat = parseFloat(paramLatLng.split("/")[0]);
-    let lng = parseFloat(paramLatLng.split("/")[1]);
-    console.log(`${lat.toString()}, ${lng.toString()}`);
-
+    const lat = parseFloat(paramLatLng.split("/")[0]);
+    const lng = parseFloat(paramLatLng.split("/")[1]);
     if (!isNaN(lat) && !isNaN(lng)) {
         opponentLatLng = [lat, lng];
     }
@@ -31,19 +32,46 @@ window.onload = () => {
         opponentLatLng = undefined;
     }
 
-    quickStart();
-    initShareButton();
-};
-
-function quickStart() {
     // Leafletマップオブジェクトを作成
-    let map = L.map("map", {doubleClickZoom: false});
-
+    map = L.map("map", {doubleClickZoom: false});
+    
     // 現在地を地図で表示
-    map.locate({setView: true, maxZoom: 7});
+    map.locate({setView: true, maxZoom: defaultZoomLvl});
+
+    // タイルレイヤーを設定
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: defaultZoomLvl,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    L.Control.geocoder().addTo(map);
 
     // 現在地にピン
-    map.on("locationfound", (e) => {
+    map.once("locationfound", (e) => {
+        if (!marker) {
+            marker = L.marker(e.latlng).addTo(map);
+        } else {
+            marker.setLatLng(e.latlng).addTo(map);
+        }
+
+        // 現在地情報を控える
+        currentLatLng = {lat: e.latlng.lat, lng: e.latlng.lng};
+
+        // ピンに現在時刻を表示
+        marker.bindPopup(`現在地<br>${hours()}:${minutes()}`).openPopup();
+    });
+
+    if (!navigator.geolocation) {
+        console.log("Error: Geolocation機能がありません。");
+    } else {
+        setInterval(() => {
+            navigator.geolocation.getCurrentPosition(getPosition);
+        }, 5000);
+    }
+};
+
+function quickStart(map) {
+    // 現在地にピン
+    map.once("locationfound", (e) => {
         let marker;
         if (!marker) {
             marker = L.marker(e.latlng).addTo(map);
@@ -53,7 +81,6 @@ function quickStart() {
 
         // 現在地情報を控える
         currentLatLng = {lat: e.latlng.lat, lng: e.latlng.lng};
-        console.log(currentLatLng);
 
         // ピンに現在時刻を表示
         marker.bindPopup(`現在地<br>${hours()}:${minutes()}`).openPopup();
@@ -63,22 +90,14 @@ function quickStart() {
     if (opponentLatLng != undefined) {
         L.marker(opponentLatLng).bindPopup("相手の現在地").addTo(map);
     }
-
-    // タイルレイヤーを設定
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
 }
 
 function initShareButton() {
-    const btn = document.getElementById("shareButton");
+    const btn = document.getElementById("share-button");
     btn.addEventListener("click", async () => {
         const shareData = {
             title: "現在地を共有",
             text: `経度緯度(${currentLatLng.lat}, ${currentLatLng.lng}), 時刻(${hours()}:${minutes()})`,
-            // url: `https://www.openstreetmap.org/#map=7/${currentLatLng.lat}/${currentLatLng.lng}`,
-            // url: `https://www.openstreetmap.org/search?query=${currentLatLng.lat}%2C${currentLatLng.lng}#map=7/${currentLatLng.lat}/${currentLatLng.lng}`,
             url: `https://rainyseason-rainy-days.netlify.app/ls.html#${currentLatLng.lat}/${currentLatLng.lng}`
         };
     
@@ -89,4 +108,15 @@ function initShareButton() {
             console.log(`Error: ${err}`);
         }
     });
+}
+
+function getPosition(position) {
+    if (marker){
+        map.removeLayer(marker);
+    }
+
+    marker = L.marker([
+        position.coords.latitude, 
+        position.coords.longitude
+    ]).addTo(map);
 }
